696aa41ccaf8a7307dcb10d99034d641f845dca6
Arman Dzhusupov <jusa11@yandex.ru>
2022-12-15 21:06:02 +0600

    completed the menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,28 +1,18 @@
-'use strict'
-const menuItem = Array.from(document.querySelectorAll('.menu__item'));		// массив всех пунктов меню
-const menuSub = Array.from(document.querySelectorAll('.menu_sub'));			// массив вложенных меню
+const menuItem = Array.from(document.querySelectorAll('.menu__item'));
 
-
-const getMenuClick = (index) => menuItem[index];
+const getMenuItem = (index) => menuItem[index];
 
 for (let i = 0; i < menuItem.length; i++) {
-	let getLink = getMenuClick(i);
-
-	const linkParent = getLink.querySelector('.menu_sub');		// поиск вложенного меню
-
-	getLink.onclick = () => {
-		let linkIndex = menuSub.findIndex(link => link.classList.contains('menu_active'));		// получаем индекс меню из массива вложенных меню
-		// если открыто более 1 вложенного меню найдено, удалить у последнего класс menu_active
-		if (linkIndex >= 0) {
-			menuSub[linkIndex].classList.remove('menu_active');
+	getMenuItem(i).querySelector('.menu__link').onclick = () => {
+		
+		if (getMenuItem(i).closest('.menu_main').querySelector('.menu_active')) {
+			getMenuItem(i).closest('.menu_main').querySelector('.menu_active').classList.remove('menu_active');
 		}
-		// если у пункта меню есть вложенное меню, то добавить ему menu_active, чтобы вывести его
-		if (linkParent) {
-			linkParent.classList.toggle('menu_active');
+
+		if (getMenuItem(i).closest('.menu_main').querySelector('.menu_active') === null) {
+			getMenuItem(i).querySelector('.menu_sub').classList.add('menu_active');
 			return false;
 		}
 	}
 }
-
-
 

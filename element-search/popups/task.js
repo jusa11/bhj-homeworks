192bbe8e53d285665c81f2a1e29999554dc0b977
Arman Dzhusupov <jusa11@yandex.ru>
2022-12-15 21:07:01 +0600

    completed the popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,41 +1,37 @@
 'use strict'
 /* объявление пермеменных */
-let modalMain = document.getElementById('modal_main');			// первое окно
-let modalSuccess = document.getElementById('modal_success');	// второе окно
-let showSuccess = document.querySelector('.show-success');		// кнопка сделать хорошо
-let btnSuccess = document.querySelector('.btn_success');			// кнопка Хорошо сделано!
-let modalClose = document.querySelectorAll('.modal__close');	// закрыть окно на крестик
-let arrayClose = Array.from(modalClose);								// массив крестиков из первого и второго окна
+const modalMain = document.getElementById('modal_main');							// первое окно
+const modalSuccess = document.getElementById('modal_success');					// второе окно
+const modalClose = Array.from(document.querySelectorAll('.modal__close'));	// закрыть окно на крестик
+const modalBtn = Array.from(document.querySelectorAll('.btn'));					// кнопки
 
 
-// функции показа, перехода и удаления окон
+/* функции показа, перехода и удаления окон */
 const modalActive = () => modalMain.classList.add('modal_active');		// при загрузке страницы показ первого окна
 const successShow = () => modalSuccess.classList.add('modal_active');	// показ второго окна при клике на кнопку
-const closeModal = (modal) => {
-	modal.classList.remove('modal_active');
-}
 
-// показ первого окна
+/* показ первого окна */
 modalActive();
 
-// при нажатии на крестик закрывает первое окно
-arrayClose[0].onclick = () => {
-	closeModal(modalMain);
+const getModalClose = (index) => modalClose[index];
+const getModalBtn = (index) => modalBtn[index];
+
+/* закрытие окна на крестик */
+for (let i = 0; i < modalClose.length; i++) {
+	getModalClose(i).onclick = () => getModalClose(i).closest('.modal').classList.remove('modal_active');
 }
 
-// при нажатии на крестик закрывает второе окно
-arrayClose[2].onclick = () => {
-	closeModal(modalMain);
-	closeModal(modalSuccess);
+/* закрытие окна на кнопку */
+for (let i = 0; i < modalBtn.length; i++) {
+	getModalBtn(i).onclick = () => {
+		if (getModalBtn(i) === modalBtn[0]) {
+			getModalBtn(i).closest('.modal').classList.remove('modal_active');
+			successShow();
+		}
+		else {
+			getModalBtn(i).closest('.modal').classList.remove('modal_active');
+		}
+	}
 }
 
-// при нажатии на кнопкe Хорошо сделано! закрывает второе окно
-btnSuccess.onclick = () => {
-	closeModal(modalMain);
-	closeModal(modalSuccess);
-}
 
-// при нажатии на кнопку переход ко второму окну
-showSuccess.onclick = () => {
-	successShow();
-}
